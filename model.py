5dabddf9bbf128f4987efa8894bfc41eac3c1dcc
juans <juan.sosa76880@gmail.com>
2025-11-30 22:20:29 -0600

    current

--- model.py
+++ model.py
@@ -1,10 +1,39 @@
 import torch
+from tresnet import TResnetM
+from datasets import load_dataset
+import os
+from time import time
+from tqdm import tqdm
+os.environ["HF_TOKEN"] = "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"
 
-if not torch.cuda.is_available():
-    print('CUDA is not available. Aborting')
-    raise SystemExit
-else:
-    print('CUDA is available. Proceeding')
-    device = torch.device('cuda')
+if __name__ == '__main__':
+    if not torch.cuda.is_available():
+        print('CUDA is not available. Aborting')
+        raise SystemExit
+    else:
+        print('CUDA is available. Proceeding')
+        device = torch.device('cuda')
 
-resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50')
+    print('Loading Model')
+    start=time()
+    model=TResnetM({'num_classes':10, 'remove_aa_jit':False})
+    end=time()
+    print("Finished Loading, took: ", end-start)
+
+    print('Loading Dataset')
+    start=time()
+    ds = load_dataset("imagenet-1k", split='train', streaming=True)
+    ds = ds.shuffle()
+    end=time()
+    print('Finished Loading, took: ',end-start)
+
+    #print(next(iter(ds)))
+    print('Starting Processing')
+    start=time()
+    for sample in ds.take(10000):
+        dataloader=torch.utils.data.DataLoader(list(sample['image']), batch_size=64, num_workers=4)
+        result=model(sample)
+        print(result)
+    end=time()
+    print('Finished Processing, took: ', end-start)
+    #print('Result: ',result)
